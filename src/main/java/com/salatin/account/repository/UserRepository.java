e20be783b3d35b6e44bf226b9ff5ed709d771bf4
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-04 16:38:59 +0300

    user repository was added

---
+++ src/main/java/com/salatin/account/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.salatin.account.repository;
+
+import com.salatin.account.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findUserByEmail(String email);
+}

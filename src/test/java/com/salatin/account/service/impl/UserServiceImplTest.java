e67e5f338eddcbfba80c05cf58464efb4eef3a8f
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-28 13:32:09 +0300

    in UserServiceImplTest tests were fixed and test on findById added

--- src/test/java/com/salatin/account/service/impl/UserServiceImplTest.java
+++ src/test/java/com/salatin/account/service/impl/UserServiceImplTest.java
@@ -1,17 +1,19 @@
 package com.salatin.account.service.impl;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -24,7 +26,6 @@ class UserServiceImplTest {
     private UserServiceImpl userService;
     @Mock
     private UsersResource usersResource;
-
     private UserRepresentation userRepresentation;
 
     @BeforeEach
@@ -39,7 +40,21 @@ class UserServiceImplTest {
         Mockito.when(usersResource.search(EMAIL))
             .thenReturn(Collections.singletonList(userRepresentation));
 
-        assertNotNull(userService.save(userRepresentation));
+        StepVerifier.create(userService.save(userRepresentation))
+            .expectNextCount(1)
+            .verifyComplete();
+    }
+
+    @Test
+    void findByIdWhenValidIdThenReturnUserRepresentation() {
+        UserResource userResource = mock(UserResource.class);
+
+        Mockito.when(usersResource.get(ID)).thenReturn(userResource);
+        Mockito.when(userResource.toRepresentation()).thenReturn(userRepresentation);
+
+        StepVerifier.create(userService.findById(ID))
+            .expectNextCount(1)
+            .verifyComplete();
     }
 
     @Test
@@ -47,7 +62,9 @@ class UserServiceImplTest {
         Mockito.when(usersResource.search(EMAIL))
             .thenReturn(Collections.singletonList(userRepresentation));
 
-        assertNotNull(userService.findByEmail(EMAIL));
+        StepVerifier.create(userService.findByEmail(EMAIL))
+            .expectNextCount(1)
+            .verifyComplete();
     }
 
     @Test
@@ -55,6 +72,8 @@ class UserServiceImplTest {
         Mockito.when(usersResource.searchByAttributes(PHONE_ATTRIBUTE + PHONE_NUMBER))
             .thenReturn(Collections.singletonList(userRepresentation));
 
-        assertNotNull(userService.findByPhoneNumber(PHONE_NUMBER));
+        StepVerifier.create(userService.findByPhoneNumber(PHONE_NUMBER))
+            .expectNextCount(1)
+            .verifyComplete();
     }
 }
